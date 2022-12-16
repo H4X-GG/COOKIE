333ef3a3483fae5bb837deae5816d8ad45535049
MEHEDI HASAN <107046336+H4X-GG@users.noreply.github.com>
2022-12-16 23:45:51 +0600

    Update cooke.py

--- cooke.py
+++ cooke.py
@@ -6,9 +6,9 @@ print('\033[1;36m[•] \033[1;32mChecking Updates...')
 
 os.system('xdg-open https://facebook.com/groups/590005482506415/')
 
-import RANDOM
+import cookie 
 
-DUMP.H4X()
+cookie.logo()
 
  
 
